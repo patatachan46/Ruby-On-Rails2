0df106cc05cec3154ac586ed1c4af9e317b34dfc
Soshi Koike <ls600hlsoushi@yahoo.co.jp>
2019-02-27 21:37:14 +0900

    final commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_125824) do
+ActiveRecord::Schema.define(version: 2019_02_27_092421) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -25,7 +32,7 @@ ActiveRecord::Schema.define(version: 2019_02_25_125824) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_name"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
